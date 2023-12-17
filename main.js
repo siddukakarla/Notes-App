8db7c9ddd01625b82fff6a53823d4c4e7439a13f
siddukakarla <139095476+siddukakarla@users.noreply.github.com>
2023-12-17 08:30:25 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+showNotes();
+let addBtn = document.getElementById('addBtn')
+addBtn.addEventListener('click', function (e) {
+
+    let addTxt = document.getElementById('addTxt');
+    let addTitle = document.getElementById('addTitle');
+    console.log(addTitle);
+    if (addTxt.value.length != 0) {
+        let notes = localStorage.getItem('notes')
+        if (notes == null) {
+            notesObj = [];
+        }
+        else {
+            notesObj = JSON.parse(notes)
+        }
+        let myObj = {
+              title: addTitle.value,
+              text: addTxt.value
+        }
+        notesObj.push(myObj);
+        localStorage.setItem('notes', JSON.stringify(notesObj));
+        addTxt.value = "";
+        addTitle.value = "";
+        showNotes();
+    }
+})
+
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes)
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="noteCard card mx-3 my-3" style="width: 18rem;">
+
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.text}</p>
+                    <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                </div>
+            </div>
+        
+              `
+    });
+    let addCard = document.getElementById('notes');
+    if (html.length != 0) {
+        addCard.innerHTML = html;
+    }
+    else {
+        addCard.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes`;
+
+    }
+}
+function deleteNote(id) {
+  
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes)
+    }
+    notesObj.splice(id, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes();
+}
+let search = document.getElementById('searchTxt');
+search.addEventListener('input', function (element) {
+    
+    let inputVal=search.value.toLowerCase();
+    let noteCards=document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt=element.getElementsByTagName('p')[0];
+        if(cardTxt.innerText.includes(inputVal))
+        {
+             element.style.display="block";
+        }
+        else
+        {
+            element.style.display="none";
+        }
+    })
+})
